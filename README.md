d3cdef24d331e8f93249f0290dac9e0bd1db3b97
Steffen Macke <sdteffen@sdteffen.de>
2013-12-01 18:31:53 +0100

    A little bit of documentation.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 epanet-haskell
 ==============
 
-Call the EPANET toolkit via Haskell's Foreign Function Interface
+Call the EPANET toolkit via Haskell's Foreign Function Interface.
+
+See [epanet.de/developer/haskell.html](http://epanet.de/developer/haskell.html) for details.
+
+GNU make, gcc and ghc are required to build the example.
+
+Run "make" to build the example.
+
+Execute "./epanet_haskell_example" to run the example.
+
+See LICENSE for licensing details.
--- epanet_haskell_example.hs
+++ epanet_haskell_example.hs
@@ -1,3 +1,23 @@
+--
+-- epanet_haskell_example.hs: Example to illustrate EPANET Toolkit usage in Haskell
+--
+-- Author:
+--   Steffen Macke (sdteffen@sdteffen.de)
+--
+-- Copyright (C) 2013 Steffen Macke (http://epanet.de)
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, version 3 of the License
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http:--www.gnu.org/licenses/>.
+--
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Main where
 
